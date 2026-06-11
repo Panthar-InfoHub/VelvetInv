b75c11904712902089f2c4d2955b1e789ab3d003
sharadsavy <sharad.panthar@adgrid.ai>
2026-06-11 23:03:43 +0530

    chore(android): Bump version code and version name
    This commit updates the Android application versioning in the build configuration to prepare for a new release.
    
    **Key Changes:**
    
    *   **`build.gradle.kts`:**
        *   Incremented `versionCode` to 15.
        *   Updated `versionName` to 1.2.6.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
         applicationId = "org.sharad.velvetinvestment"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 14
-        versionName = "1.2.5"
+        versionCode = 15
+        versionName = "1.2.6"
     }
     packaging {
         resources {
