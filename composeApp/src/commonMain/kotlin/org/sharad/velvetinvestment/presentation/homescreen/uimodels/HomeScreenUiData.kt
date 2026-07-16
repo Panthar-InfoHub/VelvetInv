6d0bebb5dd3ef60a8a9ac8e3e28178962b46647c
sharadsavy <sharad.panthar@adgrid.ai>
2026-07-16 22:09:37 +0530

    removed unused directive

--- composeApp/src/commonMain/kotlin/org/sharad/velvetinvestment/presentation/homescreen/uimodels/HomeScreenUiData.kt
+++ composeApp/src/commonMain/kotlin/org/sharad/velvetinvestment/presentation/homescreen/uimodels/HomeScreenUiData.kt
@@ -1,8 +1,6 @@
 package org.sharad.velvetinvestment.presentation.homescreen.uimodels
 
-import org.sharad.velvetinvestment.domain.models.home.FireReportSummaryDomain
 import org.sharad.velvetinvestment.domain.models.home.GoalsSummaryDomain
-import org.sharad.velvetinvestment.domain.models.home.KYCCompletion
 import org.sharad.velvetinvestment.domain.models.home.UserWorthCardDomain
 
 data class HomeScreenUiData(
--- composeApp/src/commonMain/kotlin/org/sharad/velvetinvestment/presentation/portfolio/compose/EmptyFundScreen.kt
+++ composeApp/src/commonMain/kotlin/org/sharad/velvetinvestment/presentation/portfolio/compose/EmptyFundScreen.kt
@@ -3,25 +3,16 @@ package org.sharad.velvetinvestment.presentation.portfolio.compose
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
-import org.sharad.emify.core.ui.theme.titleColor
-import org.sharad.velvetinvestment.shared.compose.AppButton
-import org.sharad.velvetinvestment.shared.theme.titlesStyle
 import velvet.composeapp.generated.resources.Res
-import velvet.composeapp.generated.resources.empty_funds_ic
-import velvet.composeapp.generated.resources.graphics_portfolio_empty
 import velvet.composeapp.generated.resources.ic_empty_notification
 
 @Composable
